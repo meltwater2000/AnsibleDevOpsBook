2fcdd46de2741b6778749fda6bed0e0d135049ee
Ubuntu <ubuntu@ip-172-31-17-33.eu-west-1.compute.internal>
2019-12-11 12:54:30 +0000

    Get network and status info

--- info-aws.sh
+++ info-aws.sh
@@ -1,8 +1,15 @@
 # Usage: sh info-aws.sh <aws-instance-id>
+echo ==Everything
 aws ec2 describe-instances --instance-ids $1
+echo ==Instance Info
 aws ec2 describe-instances --instance-ids $1 \
-  --query 'Reservations[*].Instances[*].{Instance:InstanceId:PublicDnsName}' \
+  --query 'Reservations[*].Instances[*].{Instance:InstanceId,Instance:NetworkInterfaces}' \
   --output json
+echo ==Netowrk Info
 aws ec2 describe-instances --instance-ids $1 \
-  --query 'Reservations[*].Instances[*].{Instance:InstanceId,State:Name}' \
+  --query 'Reservations[*].Instances[*].NetworkInterfaces[*].{NetworkInterfaces:Association}' \
+  --output json
+echo ==Status
+aws ec2 describe-instances --instance-ids $1 \
+  --query 'Reservations[*].Instances[*].{Instance:State}' \
   --output json
